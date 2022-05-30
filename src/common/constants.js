c5143aa225680f6f1768cefedc406bd69ab2e102
ameeee <amena@auxi.ai>
2022-05-30 11:39:38 +0300

    comment

--- src/common/constants.js
+++ src/common/constants.js
@@ -40,7 +40,7 @@ export const POPOVER_HTML = (className = '') => `
     <div class="${CLASS_POPOVER_TITLE}">Popover Title</div>
     <div class="${CLASS_POPOVER_DESCRIPTION}">Popover Description</div>
     <div class="driver-clearfix ${CLASS_POPOVER_FOOTER}">
-      <button class="${CLASS_CLOSE_BTN}">Close</button>
+      <button class="${CLASS_CLOSE_BTN}" onClick="CloseT()">Close</button>
       <span class="driver-btn-group ${CLASS_NAVIGATION_BTNS}">
         <button class="${CLASS_PREV_STEP_BTN}">&larr; Previous</button>
         <button class="${CLASS_NEXT_STEP_BTN}">Next &rarr;</button>
